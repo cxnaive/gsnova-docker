bdc6d7bba3bd4122be855eda2cf8dbd62b1ed0ec
cxnaive <1012033254@qq.com>
2016-10-04 09:15:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ cd /vps/
 ./gsnova_vps_linux_amd64 >gsnova.log 2>&1 &
 echo "gsnova started."
 cd /root/kcptun/
-./server_linux_amd64 -l :29900 -t 127.0.0.1:8080 -key test -mtu 1400 -sndwnd 2048 -rcvwnd 2048 -mode fast2 > kcptun.log 2>&1 & 
+./server_linux_amd64 -t "127.0.0.1:8080" -l ":4000" -mode normal > kcptun.log 2>&1 & 
 echo "Kcptun started."
